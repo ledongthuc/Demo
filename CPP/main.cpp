c709ba0ec953010041f4d4e6f1cc8fa938491de0
ledongthuc <info@thuc.com.vn>
2012-09-18 18:19:57 +0700

    Example 2 of CPP

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -4,9 +4,13 @@ using namespace std;
 
 int main()
 {
-    /*
-     * Example 1
-     */
-    cout << "Hello world!" << endl;
+//  Example 1: Hello World
+//    cout << "Hello world!" << endl;
+
+//  Example 2: Statement
+    int x;
+    x = 5;
+    cout << x << endl;
+
     return 0;
 }
